aa969c4db3d2546016cd08094640dfb2e09bc173
Cristina Rueda <crisruedap@gmail.com>
2022-07-31 02:41:41 -0500

    Auth

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+  def index
+    #flash[:notice] = "Welcome Rails App"
+    #flash[:alert] = "Exist an error with Rails App"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resource :users
+  # Defines the root path route ("/")
+  root "welcome#index"
   
+  # Auth
+  get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  get "logout", to: "sessions#destroy"
+  resource :users, only: [:new, :create]
+
+  # Auth with Google
   get 'login', to: 'logins#new'
   get 'login/create', to: 'logins#create', as: :create_login
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
